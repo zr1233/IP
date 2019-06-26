c126672c6fb0812dd33dd81a62c79b28a1d28a5d
liwei <liw>
2019-06-26 10:12:32 +0800

    weather

--- index.php
+++ index.php
@@ -25,6 +25,22 @@ $data = json_decode($data, true);
 $country = $data['data']['country']; 
 $region = $data['data']['region']; 
 $city = $data['data']['city'];
+
+$url="http://api.map.baidu.com/telematics/v3/weather?output=json&ak=A4ddc10f0a87914d68e99d8e267fc749location=武汉";
+
+$curl = curl_init(); 
+curl_setopt($curl, CURLOPT_URL, $url); 
+curl_setopt($curl, CURLOPT_HEADER, 0);  
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);  
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);  
+curl_setopt($curl, CURLOPT_ENCODING, '');  
+curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);  
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);  
+$data = curl_exec($curl);
+$data = json_decode($data, true);
+$weather = $data['results']['weather_data'][0]['weather'].' '.$data['results']['weather_data'][0]['wind'];
+
 //定义颜色
 $black = ImageColorAllocate($im, 0,0,0);//定义黑色的值
 $red = ImageColorAllocate($im, 255,0,0);//红色
@@ -36,11 +52,10 @@ if (strlen($address) == 2) {
 //输出
 imagettftext($im, 16, 0, 10, 40, $black, $font,'欢迎您,来自'.$address.'的朋友');
 imagettftext($im, 16, 0, 10, 72, $black, $font, '今天是'.date('Y年n月j日')."  星期".$weekarray[date("w")]);//当前时间添加到图片
-imagettftext($im, 16, 0, 10, 104, $black, $font,'您的IP是:'.$ip.'  :('.$counter.')');//ip
-imagettftext($im, 15, 0, 10, 140, $black, $font,'您使用的是'.$os.'操作系统');
-imagettftext($im, 15, 0, 10, 175, $black, $font,'您使用的是'.$bro);
-imagettftext($im, 16, 0, 10, 205, $red, $font,$get); 
-// imagettftext($im, 15, 0, 10, 200, $red, $font,'祝您网上冲浪愉快,大家携手,共建和谐网络.'); 
+imagettftext($im, 16, 0, 10, 104, $black, $font,$weather );//ip
+imagettftext($im, 15, 0, 10, 140, $black, $font,'您的IP是:'.$ip.'  :('.$counter.')' );
+imagettftext($im, 15, 0, 10, 175, $black, $font,'您使用的是'.$os.'操作系统');
+imagettftext($im, 16, 0, 10, 205, $red, $font,$get);  
 ImageGif($im);
 ImageDestroy($im);
 ?>
